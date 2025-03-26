ed94f589ab449f0f78451c4668c509046e6afa81
angelbarros14 <social.angelbarros@gmail.com>
2025-03-26 23:33:57 +0800

    form submission and push data to array

--- script.js
+++ script.js
@@ -14,20 +14,26 @@ function addBookToLibrary(book) {
 
 const book1 = new Book("All the Bright Places", "Jennifer Niven", 388, true);
 addBookToLibrary(book1);
+displayBook(book1);
+
 
 function displayBook() {
     const books = document.getElementById("books");
+
+    while(books.firstChild) {
+        books.removeChild(books.firstChild);
+    }
 
     myLibrary.forEach(book => {
         const divBook = document.createElement("div");
         divBook.classList.add("newBook");
 
         const title = document.createElement("span");
-        title.textContent= `${book.title}`;
+        title.textContent= `"${book.title}"`;
         divBook.appendChild(title);
 
         const author = document.createElement("span");
-        author.textContent= `${book.author}`;
+        author.textContent= `by ${book.author}`;
         divBook.appendChild(author);
 
         const pages = document.createElement("span");
@@ -40,6 +46,7 @@ function displayBook() {
         divBook.appendChild(statusButton);
 
         const removeButton = document.createElement("button");
+        removeButton.classList.add("removeButton")
         removeButton.textContent = "Remove";
         divBook.appendChild(removeButton);
 
@@ -47,17 +54,43 @@ function displayBook() {
     })
 }
 
-displayBook()
+const dialog = document.querySelector("dialog");
 
-function inputBook() {
-    const dialog = document.querySelector("dialog");
+function showDialog() {
     const addButton = document.querySelector(".addButton");
-
+    
     addButton.addEventListener("click", () => {
         dialog.showModal();
     })
 
 }
 
-inputBook()
+showDialog()
 
+function closeDialog(event) {
+    if (!event.target.contains(dialog)) return;
+    dialog.close();
+}
+
+document.addEventListener("click", closeDialog)
+
+function addBook() {
+    const form = document.querySelector("form")
+    const close = document.querySelector("#close");
+
+    form.addEventListener("submit", () => {
+        const title = document.querySelector("#title").value;
+        const author = document.querySelector("#author").value;
+        const pages = document.querySelector("#pages").value;
+        const read = document.querySelector("#read").checked;
+
+    title && author && pages ? addBookToLibrary(new Book(title, author, pages, read)) : alert("Please fill out all fields");
+    displayBook()
+    form.reset();
+    })
+}
+
+addBook()
+
+
+console.log(myLibrary)
